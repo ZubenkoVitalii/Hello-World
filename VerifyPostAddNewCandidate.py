82fbb7eebe23fe38cda5b499561e0ac798e6daa7
ZubenkoVitalii <zubenkovitalii@gmail.com>
2014-12-11 18:58:51 +0200

    Create VerifyPostAddNewCandidate.py

---
+++ VerifyPostAddNewCandidate.py
@@ -0,0 +1,38 @@
+import urllib.request
+import json
+
+check_data = {'name': 'Vitalii', 'position': 'QA Engineer'}
+
+# convert data to JSON
+json_data = json.dumps(check_data)
+# convert str to bytes (ensure encoding is OK)
+post_data = json_data.encode('utf-8')
+# we should also say the JSON content type header
+headers = {'Content-Type': 'application/json'}
+# now do the request for a url
+req = urllib.request.Request('http://qainterview.cogniance.com/candidates', post_data, headers)
+# send the request
+res = urllib.request.urlopen(req)
+
+if res.getcode() != 201:
+    print('Candidate is not added, wrong http response code, != 201')
+
+json_resp = res.read().decode("cp1251")
+cand_resp = json.loads(json_resp)
+
+if cand_resp and type(cand_resp) == dict and 'candidate' in cand_resp.keys() and 'id' in cand_resp['candidate'].keys():
+    print('Candidate added, check if name and position are correct')
+    flag = True
+    for k in check_data.keys():
+        if k not in cand_resp['candidate'].keys() or cand_resp['candidate'][k] != check_data[k]:
+            flag = False
+            break
+    if flag:
+        print("Candidate data added correctly")
+    else:
+        print("Wrong candidate data")
+else:
+    print('Candidate is not added')
+    
+Candidate added, check if name and position are correct
+Candidate data added correctly
