f5e460c67f451381a97c7934915613d2bb818628
ZubenkoVitalii <zubenkovitalii@gmail.com>
2014-12-11 18:14:47 +0200

    Update Get_candidates_with_id.py

--- Get_candidates_with_id.py
+++ Get_candidates_with_id.py
@@ -2,15 +2,13 @@ Python 3.4.2rc1 (v3.4.2rc1:8711a0951384, Sep 21 2014, 21:21:43) [MSC v.1600 64 b
 Type "copyright", "credits" or "license()" for more information.
 >>> import json
 >>> from urllib.request import urlopen
->>> response = urlopen('http://qainterview.cogniance.com/candidates')
->>> html = response.read().decode('utf-8')
->>> candidates = json.loads(html)['candidates']
->>> for item in candidates:
-	if item['id'] == 33:
-		print(item)
-
-		
-{'id': 33, 'name': 'name_test', 'position': 'pos_test'}
->>> response.close
+>>> res = urlopen("http://qainterview.cogniance.com/candidates/37")
+>>> print(res.read().decode("cp1251"))
+{
+  "candidate": {
+    "id": 22, 
+    "name": "Dmitrii", 
+    "position": "QA_intern"
+>>>res.close
 <bound method HTTPResponse.close of <http.client.HTTPResponse object at 0x0000000003A805C0>>
 >>> 
