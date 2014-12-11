da0e93aec73b69b1d9f308a0a08485a4c9dc4458
ZubenkoVitalii <zubenkovitalii@gmail.com>
2014-12-11 18:16:56 +0200

    Update Get_candidates_with_id.py

--- Get_candidates_with_id.py
+++ Get_candidates_with_id.py
@@ -1,6 +1,5 @@
 Python 3.4.2rc1 (v3.4.2rc1:8711a0951384, Sep 21 2014, 21:21:43) [MSC v.1600 64 bit (AMD64)] on win32
 Type "copyright", "credits" or "license()" for more information.
->>> import json
 >>> from urllib.request import urlopen
 >>> res = urlopen("http://qainterview.cogniance.com/candidates/37")
 >>> print(res.read().decode("cp1251"))
@@ -10,5 +9,5 @@ Type "copyright", "credits" or "license()" for more information.
     "name": "Dmitrii", 
     "position": "QA_intern"
 >>>res.close
-<bound method HTTPResponse.close of <http.client.HTTPResponse object at 0x0000000003A805C0>>
+<bound method HTTPResponse.close of <http.client.HTTPResponse object at 0x0000000003A99320>>
 >>> 
