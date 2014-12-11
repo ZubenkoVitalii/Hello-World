e4324f91680e532845dd56008f60cd3699d9f9c3
ZubenkoVitalii <zubenkovitalii@gmail.com>
2014-12-11 19:01:51 +0200

    Update GET_list_of_candidates.py

--- GET_list_of_candidates.py
+++ GET_list_of_candidates.py
@@ -1,8 +1,8 @@
 Python 3.4.2rc1 (v3.4.2rc1:8711a0951384, Sep 21 2014, 21:21:43) [MSC v.1600 64 bit (AMD64)] on win32
 Type "copyright", "credits" or "license()" for more information.
->>> from urllib.request import urlopen
->>> res = urlopen("http://qainterview.cogniance.com/candidates")
->>> print (res.read().decode("cp1251"))
+from urllib.request import urlopen
+res = urlopen("http://qainterview.cogniance.com/candidates")
+print (res.read().decode("cp1251"))
 {
   "candidates": [
     {
@@ -4592,6 +4592,6 @@ Type "copyright", "credits" or "license()" for more information.
     }
   ]
 }
->>> res.close
+res.close
 <bound method HTTPResponse.close of <http.client.HTTPResponse object at 0x000000000364AC88>>
 >>> 
