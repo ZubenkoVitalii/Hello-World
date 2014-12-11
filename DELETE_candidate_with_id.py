d87200a831b00eb04e7482eb7d6d3b2e43fe2595
ZubenkoVitalii <zubenkovitalii@gmail.com>
2014-12-11 18:59:45 +0200

    Update DELETE_candidate_with_id.py

--- DELETE_candidate_with_id.py
+++ DELETE_candidate_with_id.py
@@ -1,8 +1,8 @@
 Python 3.4.2rc1 (v3.4.2rc1:8711a0951384, Sep 21 2014, 21:21:43) [MSC v.1600 64 bit (AMD64)] on win32
 Type "copyright", "credits" or "license()" for more information.
->>> import urllib.request
->>> request_url = 'http://qainterview.cogniance.com/candidates/6'
->>> request = urllib.request.Request(request_url)
->>> request.get_method = lambda: 'DELETE'
->>> urllib.request.urlopen(request)
+import urllib.request
+request_url = 'http://qainterview.cogniance.com/candidates/6'
+request = urllib.request.Request(request_url)
+request.get_method = lambda: 'DELETE'
+urllib.request.urlopen(request)
 <http.client.HTTPResponse object at 0x0000000003A34EF0>
