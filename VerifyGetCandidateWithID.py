ce83c575141e41889920387c691cf5c42940124c
ZubenkoVitalii <zubenkovitalii@gmail.com>
2014-12-12 15:19:44 +0200

    Update VerifyGetCandidateWithID.py

--- VerifyGetCandidateWithID.py
+++ VerifyGetCandidateWithID.py
@@ -1,14 +1,14 @@
->>> import json
->>> from urllib.request import urlopen
->>> id = '25'
->>> response = urlopen('http://qainterview.cogniance.com/candidates/' + id)
->>> if response.getcode() !=200:
+ import json
+ from urllib.request import urlopen
+ id = '25'
+ response = urlopen('http://qainterview.cogniance.com/candidates/' + id)
+ if response.getcode() !=200:
 	print('Candidate is not exist, wrong http response code != 200')
 
 	
->>> json_resp = response.read().decode("cp1251")
->>> cand_resp = json.loads(json_resp)
->>> if cand_resp and type(cand_resp) == dict and 'candidate' in cand_resp.keys() and 'id' in cand_resp['candidate'].keys():
+ json_resp = response.read().decode("cp1251")
+ cand_resp = json.loads(json_resp)
+ if cand_resp and type(cand_resp) == dict and 'candidate' in cand_resp.keys() and 'id' in cand_resp['candidate'].keys():
 	received_id = cand_resp['candidate']['id'].__str__()
 	print('Good response from the server with candidate info')
 	print('Check if we received correct info for candidate with id=' + id)
@@ -23,4 +23,4 @@ else:
 Good response from the server with candidate info
 Check if we received correct info for candidate with id=25
 Received info for candidate with id=22, but not 25
->>> 
+
