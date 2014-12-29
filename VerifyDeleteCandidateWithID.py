e15aa44eaa8b976c46504be7e60a3899dfe19ee9
ZubenkoVitalii <zubenkovitalii@gmail.com>
2014-12-29 20:41:50 +0200

    Update VerifyDeleteCandidateWithID.py

--- VerifyDeleteCandidateWithID.py
+++ VerifyDeleteCandidateWithID.py
@@ -1,11 +1,12 @@
 import urllib.request
+# now do the request for a url
 id = '43'
-
 request_url = ('http://qainterview.cogniance.com/candidates/' + id)
 request = urllib.request.Request(request_url)
 request.get_method = lambda: 'DELETE'
+# send the request
 res = urllib.request.urlopen(request)
-
+# analysis of response code
 if res.getcode() !=200:
     print('Candidate is not deleted, wrong http response code != 200')
 
