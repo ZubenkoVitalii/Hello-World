b80d24ca2d589592cf1aa9221e2ff19bb8f96b04
ZubenkoVitalii <zubenkovitalii@gmail.com>
2014-12-30 12:54:51 +0200

    Update DELETE_candidate_with_id.py

--- DELETE_candidate_with_id.py
+++ DELETE_candidate_with_id.py
@@ -1,9 +1,11 @@
 Python 3.4.2rc1 (v3.4.2rc1:8711a0951384, Sep 21 2014, 21:21:43) [MSC v.1600 64 bit (AMD64)] on win32
 Type "copyright", "credits" or "license()" for more information.
+# Include library urllib.request
 import urllib.request
-# now do the request for a url
+# Read url into variable
 request_url = 'http://qainterview.cogniance.com/candidates/6'
 request = urllib.request.Request(request_url)
+# Delete item from variable
 request.get_method = lambda: 'DELETE'
 # send the request
 urllib.request.urlopen(request)
