dc4cb6d38688268d5f50a664823ed34829a78497
ZubenkoVitalii <zubenkovitalii@gmail.com>
2014-12-30 13:46:19 +0200

    Update VerifyGetCandidateWithID.py

--- VerifyGetCandidateWithID.py
+++ VerifyGetCandidateWithID.py
@@ -10,7 +10,7 @@
  cand_resp = json.loads(json_resp)
  #проверяем, есть ли не пустой ответ от сервера, это словарь и он имеет ключ candidate и в масиве ключа candidate есть ключ id 
  if cand_resp and type(cand_resp) == dict and 'candidate' in cand_resp.keys() and 'id' in cand_resp['candidate'].keys():
-	# преобразуем объект в строку и присвоим received_id
+	# Get item (string type) into variable received_id from array with index 'id'
 	received_id = cand_resp['candidate']['id'].__str__()
 	print('Good response from the server with candidate info')
 	print('Check if we received correct info for candidate with id=' + id)
