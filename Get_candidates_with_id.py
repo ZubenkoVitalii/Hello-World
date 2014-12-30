6a87b0bc2925e8199a5fb395bcbbf39de8831792
ZubenkoVitalii <zubenkovitalii@gmail.com>
2014-12-30 11:49:37 +0200

    Update Get_candidates_with_id.py

--- Get_candidates_with_id.py
+++ Get_candidates_with_id.py
@@ -1,6 +1,7 @@
 Python 3.4.2rc1 (v3.4.2rc1:8711a0951384, Sep 21 2014, 21:21:43) [MSC v.1600 64 bit (AMD64)] on win32
 Type "copyright", "credits" or "license()" for more information.
 from urllib.request import urlopen
+# send the request
 res = urlopen("http://qainterview.cogniance.com/candidates/37")
 print(res.read().decode("cp1251"))
 {
