cfdd3555460103a2a9305f891e3ceb0fed5fc92b
ZubenkoVitalii <zubenkovitalii@gmail.com>
2014-12-30 13:47:24 +0200

    Update VerifyGetListOfCandidate.py

--- VerifyGetListOfCandidate.py
+++ VerifyGetListOfCandidate.py
@@ -9,6 +9,7 @@ if response.getcode() !=200:
 json_resp = response.read().decode("cp1251")
 cand_resp = json.loads(json_resp)
 #проверяем, есть ли не пустой ответ от сервера, это словарь и он имеет ключ candidates и значение этого ключа является массивом
+# Check request from site for type (array) and value ('candidates')
 if cand_resp and type(cand_resp) == dict and 'candidates' in cand_resp.keys() and type(cand_resp['candidates']) == list:
     print('Good response from the server with candidates info')
     print('Check if we received correct info for candidates')
